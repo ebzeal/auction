f7391b614a5a7299eb4ac3853d902ea7827e59ae
ebzeal <ebenezerajayi@gmail.com>
2021-05-03 02:05:28 +0100

    using middlewares and error handlers

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,16 @@
 import {v4 as uuid} from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from 'http-errors';
+
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
-  const {title} = JSON.parse(event.body);
+  const {title} = event.body;
   const now = new Date();
 
 const auction = {
@@ -14,10 +20,16 @@ const auction = {
   createdAt: now.toISOString(),
 };
 
-await dynamodb.put({
-  TableName: process.env.AUCTIONS_TABLE_NAME,
-  Item: auction,
-}).promise();
+try {
+  await dynamodb.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction,
+  }).promise();
+} catch (error) {
+console.log("🚀 ~ file: createAuction.js ~ line 29 ~ createAuction ~ error", error);
+  throw new createError.InternalServerError(Error);
+}
+
 
   return {
     statusCode: 201,
@@ -25,6 +37,9 @@ await dynamodb.put({
   };
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())    // this parses our strings in the body hence we can remove JSON.parse from event.body
+  .use(httpEventNormalizer())  // it will prevent us from having none existing objects to reduce errors and if statements
+  .use(httpErrorHandler());  // helps handle our errors
 
 
