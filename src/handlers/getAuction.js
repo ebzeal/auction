8d6874e0c5338b7cc2f4221fd8bca75097a7fad1
ebzeal <ebenezerajayi@gmail.com>
2021-05-03 03:56:02 +0100

    ensure that bids cannot be less than or equal to highest amount refactor getAuction to reuse fetch single option

--- src/handlers/getAuction.js
+++ src/handlers/getAuction.js
@@ -5,27 +5,31 @@ import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
+export async function getAuctionBiId(id){
+  let auction;
+  try {
+    const result = await dynamodb.get({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Key: { id }
+    }).promise();
+
+    auction = result.Item;
+  } catch (error) {
+  console.log("🚀 ~ file: getAuction.js ~ line 17 ~ getAuction ~ error", error);
+    throw new createError.InternalServerError(error);
+  }
+
+  if(!auction) {
+    throw new createError.NotFound(`Auction with ID "${id}" not found`);
+  }
+ return auction;
+}
+
 async function getAuction(event, context) {
  let auction;
  const { id } = event.pathParameters;
-
- try {
-   const result = await dynamodb.get({
-     TableName: process.env.AUCTIONS_TABLE_NAME,
-     Key: { id }
-   }).promise();
-
-   auction = result.Item;
- } catch (error) {
- console.log("🚀 ~ file: getAuction.js ~ line 17 ~ getAuction ~ error", error);
-   throw new createError.InternalServerError(error);
- }
-
- if(!auction) {
-   throw new createError.NotFound(`Auction with ID "${id}" not found`);
- }
-
-  return {
+ auction = await getAuctionBiId(id);
+ return {
     statusCode: 200,
     body: JSON.stringify({ auction }),
   };
